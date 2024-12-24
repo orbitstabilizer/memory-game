e1378c5bbb8d23ddcf0a854b8a0babd301fd1eb5
Yusuf AKIN <akinyusuf17@gmail.com>
2024-12-24 14:53:44 +0300

    clean-up

--- Debug/Src/adc.d
+++
@@ -1,3 +0,0 @@
-Src/adc.o: ../Src/adc.c ../Inc/adc.h ../Inc/addresses.h
-../Inc/adc.h:
-../Inc/addresses.h:
--- Debug/Src/gpio.d
+++
@@ -1,4 +0,0 @@
-Src/gpio.o: ../Src/gpio.c ../Inc/gpio.h ../Inc/addresses.h ../Inc/time.h
-../Inc/gpio.h:
-../Inc/addresses.h:
-../Inc/time.h:
